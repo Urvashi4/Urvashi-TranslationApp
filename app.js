2f6b97c6cc71731af7e2384c3949fddb1ab44b96
Urvashi <Urvashimalik867@gmail.com>
2020-12-07 01:54:46 +0530

    Added Audio

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var inputTextarea = document.querySelector("#inputTextarea");
 var outputTextarea = document.querySelector("#outputTextarea");
 var btnRefresh = document.querySelector("#btnRefresh");
 
+var audio1 = new Audio('/audio/minion-bababa.mp3');
+var audio2 = new Audio('/audio/minion-bello.mp3');
+var audio3 = new Audio('/audio/minion-laugh.mp3');
+
+var audioArray = [audio1,audio2,audio3];
+
+function audioSelection() {
+    var i = Math.floor(Math.random() * audioArray.length);
+    var curntAudio = audioArray[i];
+    return curntAudio;
+}
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
@@ -22,6 +33,9 @@ function clickHandler() {
     .then(response => response.json())
     .then(json => outputTextarea.innerText = json.contents.translated)
     .catch(errorHandler)
+
+    var audioPlay = audioSelection();
+    audioPlay.play();
 }
 
 btnTranslate.addEventListener("click", clickHandler);
